cd9993d3b1a79de6d6edd89db3ffd3f19ae7396f
Kr-Yan <yan_k1@denison.edu>
2024-03-23 11:56:24 -0400

    updatee

--- models/book.js
+++ models/book.js
@@ -1,10 +1,22 @@
 var mongoose = require('mongoose');
+const author = require('./author');
 
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title:{type:String, required:true, maxLength:100},
+    author:{type:Schema.Types.ObjectId, ref: 'Author', required:true, maxLength:100},
+    summary:{type: String},
+    ISBN:{type:Int},
+    genre:[GenreSchema],
+    url:{type:String, required:true}
+  }
 );
+
+BookSchema.virtual('url').get(function(){
+  return '/catalog/book/' +this._id;
+})
 
 //Export model
 module.exports = mongoose.model('Book', BookSchema);
--- models/genre.js
+++ models/genre.js
@@ -3,9 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  {
+    name:{type:String, required:true, maxlength:100},
+    url:{type: String, required:true}
+  }
 );
 
+GenreSchema.virtual('url').get(function(){
+return '/catalog/book/' +this._id;
 
+})
 //Export model
 module.exports = mongoose.model('Genre', GenreSchema);
